07129714ea99e00b81876f58183e8f050a71eb67
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-06-29 01:43:40 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,3 +1,10 @@
-export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({ firstName, lastName });
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
